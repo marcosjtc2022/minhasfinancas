2cee1dcada0c35704de5929bb406f371d8a58c82
Marcos Cardoso <marcosjtc1973@gmail.com>
2022-06-10 23:19:55 -0300

    Protegendo as rotas

--- src/main/rotas.js
+++ src/main/rotas.js
@@ -5,7 +5,37 @@ import CadastroUsuario from "../views/cadastroUsuario";
 import ConsultaLancamentos from "../views/lancamentos/consulta-lancamentos";
 import CadastroLancamentos from "../views/lancamentos/cadastro-lancamentos";
 
-import { Route,Switch,HashRouter} from 'react-router-dom'
+import { Route,Switch,HashRouter, Redirect} from 'react-router-dom'
+
+const isUsuarioAutenticado = () => {
+    return true;
+}
+
+//Usando um operado destructor para pegar parte de props.
+//No caso a propriedade component que foi passada abaixo: component={ConsultaLancamentos}.
+//Component é um alias.
+//Todos os componentes começam com letra maiúscula.
+//(...) spread operator coloca todas as props junto com componentProps.
+function RotaAutenticada({component: Component, ...props}){ 
+  return (
+    //Espalha a propriedade para uma rota comum.
+    //Outra forma de renderizar o componente Router.
+     <Route {...props} render={(componentProps) =>{
+       if (isUsuarioAutenticado()){
+         return (
+             <Component {...componentProps} />
+         ) 
+        } else {
+             return (
+                 // Redireciona para uma rota.
+                 //Salva na variável state a rota que estava tentando acessar.
+                 <Redirect to={ {pathname: '/login', state: {from: componentProps.location}} } />
+             ) 
+        }  
+
+     }  }/>
+  )
+}
 
 function Rotas(){
   //HashRouter = # da url
@@ -13,13 +43,14 @@ function Rotas(){
     return (
         <HashRouter> 
             <Switch>
-              <Route path="/home" component={Home} />
+              //Path e componente renderizado. 
               <Route path="/login" component={Login} />
               <Route path="/cadastro-usuarios" component={CadastroUsuario} />
-              <Route path="/consulta-lancamentos" component={ConsultaLancamentos} />
+              <RotaAutenticada path="/home" component={Home} />
+              <RotaAutenticada path="/consulta-lancamentos" component={ConsultaLancamentos} />
               //Interrogação no fim determina que o parâmetro é opcional.
               //Isso evita erro!
-              <Route path="/cadastro-lancamentos/:id?" component={CadastroLancamentos} />
+              <RotaAutenticada path="/cadastro-lancamentos/:id?" component={CadastroLancamentos} />
               
               
             </Switch>
--- src/views/home.js
+++ src/views/home.js
@@ -57,12 +57,12 @@ class Home extends React.Component{
                 <p className="lead">
                     <a className="btn btn-primary btn-lg" 
                     href="#/cadastro-usuarios" role="button">
-                        <i className="fa fa-users"></i> 
+                        <i className="pi pi-users"></i> 
                     Cadastrar Usuário
                     </a>
                     <a className="btn btn-danger btn-lg" 
                     href="#/cadastro-lancamentos" 
-                    role="button"><i className="fa fa-users"></i>
+                    role="button"><i className="pi pi-money-bill"></i>
                     Cadastrar Lançamento</a>
                 </p>
             </div>
